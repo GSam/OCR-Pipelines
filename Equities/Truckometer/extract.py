a37d25c8392c67296d65b63fda2b39a993b4e874
GSam <1924133+GSam@users.noreply.github.com>
2018-04-21 21:48:52 +1200

    Basic parsing using lxml

--- Equities/Truckometer/extract.py
+++ Equities/Truckometer/extract.py
@@ -1,1 +1,8 @@
+from lxml import etree
+import requests
 
+TRUCKOMETER_URL = 'https://www.anz.co.nz/about-us/economic-markets-research/truckometer/'
+contents = requests.get(TRUCKOMETER_URL)
+
+parser.etree.HTMLParser()
+etree.fromstring(content.text, parser)
