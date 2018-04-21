92b1bd9cc18a7f2705aeb3bf7c9bbe2068cfdd9f
Garming Sam <garming_sam@outlook.com>
2018-04-21 10:44:57 +0000

    user agent

--- Equities/Truckometer/extract.py
+++ Equities/Truckometer/extract.py
@@ -2,7 +2,11 @@ from lxml import etree
 import requests
 import urlparse
 import urllib
-import re, os
+import re, os, sys
+
+num_to_parse = -1
+if len(sys.argv) > 1:
+    num_to_parse = int(sys.argv[1])
 
 TRUCKOMETER_URL = 'https://www.anz.co.nz/about-us/economic-markets-research/truckometer/'
 content = requests.get(TRUCKOMETER_URL)
@@ -13,12 +17,23 @@ tree = etree.fromstring(content.text, parser)
 results = tree.xpath("//a[re:match(@href, 'ANZ-Truckometer-[\\d]+.pdf')]",
                      namespaces={"re": "http://exslt.org/regular-expressions"})
 
+files = []
+urllib.URLopener.version = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0'
+
 print 'URL available:'
 for url in results:
     print urlparse.urljoin(TRUCKOMETER_URL, url.attrib['href'])
 
     filename = re.search('ANZ-Truckometer-[\\d]+.pdf', url.attrib['href']).group(0)
+    files.append(filename)
+
     if os.path.exists(filename):
         continue
 
     urllib.urlretrieve(urlparse.urljoin(TRUCKOMETER_URL, url.attrib['href']), filename)
+
+for f in reversed(sorted(files)):
+    if num_to_parse == 0:
+        break
+
+    num_to_parse -= 1
