06fd693e1754ac5f2a4a07a2160f21d5d70f584f
Garming Sam <garming_sam@outlook.com>
2018-04-21 09:28:52 +0000

    Truckometer dependencies

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
+# Capture2Text dependencies
 sudo apt-get install -y libtesseract-dev
 sudo apt-get install -y qtbase5-dev
 sudo apt-get install -y libleptonica-dev
 
 ./build-capture2text.sh
+
+# Truckometer dependencies
+sudo apt-get install -y python-lxml
