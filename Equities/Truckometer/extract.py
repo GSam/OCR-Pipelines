b4123577a1192c6ad29df4cd6e19fd7c9f64860c
GSam <1924133+GSam@users.noreply.github.com>
2018-04-21 21:50:21 +1200

    Use regex extension for xpath

--- Equities/Truckometer/extract.py
+++ Equities/Truckometer/extract.py
@@ -5,4 +5,7 @@ TRUCKOMETER_URL = 'https://www.anz.co.nz/about-us/economic-markets-research/truc
 contents = requests.get(TRUCKOMETER_URL)
 
 parser.etree.HTMLParser()
-etree.fromstring(content.text, parser)
+tree = etree.fromstring(content.text, parser)
+
+results = tree.xpath("//a[re:match(@href, 'ANZ-Truckometer-[\\d]+.pdf')]",
+                     namespaces={"re": "http://exslt.org/regular-expressions"})
