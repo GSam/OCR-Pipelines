3e860092585b23dcdec708389dda958fcd5ab882
Garming Sam <garming_sam@outlook.com>
2018-04-21 10:06:44 +0000

    downloading pdf file

--- Equities/Truckometer/extract.py
+++ Equities/Truckometer/extract.py
@@ -1,6 +1,8 @@
 from lxml import etree
 import requests
 import urlparse
+import urllib
+import re, os
 
 TRUCKOMETER_URL = 'https://www.anz.co.nz/about-us/economic-markets-research/truckometer/'
 content = requests.get(TRUCKOMETER_URL)
@@ -14,3 +16,9 @@ results = tree.xpath("//a[re:match(@href, 'ANZ-Truckometer-[\\d]+.pdf')]",
 print 'URL available:'
 for url in results:
     print urlparse.urljoin(TRUCKOMETER_URL, url.attrib['href'])
+
+    filename = re.search('ANZ-Truckometer-[\\d]+.pdf', url.attrib['href']).group(0)
+    if os.path.exists(filename):
+        continue
+
+    urllib.urlretrieve(urlparse.urljoin(TRUCKOMETER_URL, url.attrib['href']), filename)
