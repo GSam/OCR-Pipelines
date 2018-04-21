35e49140c79fac361d4e183b878040f927a91b0c
Garming Sam <garming_sam@outlook.com>
2018-04-21 09:31:06 +0000

    stop double fetching the apt-get repos

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 # Capture2Text dependencies
-sudo apt-get install -y libtesseract-dev
-sudo apt-get install -y qtbase5-dev
-sudo apt-get install -y libleptonica-dev
+sudo apt-get install -y libtesseract-dev qtbase5-dev libleptonica-dev
 
 ./build-capture2text.sh
 
