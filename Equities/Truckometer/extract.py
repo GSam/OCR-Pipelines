19a321aac513be63850488ee27a0326dd1cd8cf1
Garming Sam <garming_sam@outlook.com>
2018-04-21 09:56:58 +0000

    listing pdf urls

--- Equities/Truckometer/extract.py
+++ Equities/Truckometer/extract.py
@@ -1,11 +1,16 @@
 from lxml import etree
 import requests
+import urlparse
 
 TRUCKOMETER_URL = 'https://www.anz.co.nz/about-us/economic-markets-research/truckometer/'
-contents = requests.get(TRUCKOMETER_URL)
+content = requests.get(TRUCKOMETER_URL)
 
-parser.etree.HTMLParser()
+parser = etree.HTMLParser()
 tree = etree.fromstring(content.text, parser)
 
 results = tree.xpath("//a[re:match(@href, 'ANZ-Truckometer-[\\d]+.pdf')]",
                      namespaces={"re": "http://exslt.org/regular-expressions"})
+
+print 'URL available:'
+for url in results:
+    print urlparse.urljoin(TRUCKOMETER_URL, url.attrib['href'])
