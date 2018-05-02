49cb4858caf3c97d84119ac78e5278b6b14c2860
GSam <1924133+GSam@users.noreply.github.com>
2018-05-02 21:35:59 +1200

    Update extract.py

--- Equities/AHTables/extract.py
+++ Equities/AHTables/extract.py
@@ -13,6 +13,7 @@ if len(sys.argv) != 3:
 
 url = url.format(sys.argv[1], sys.argv[2])
 
+# extract using convert ... pdf image.png -crop 600x400+0x600
 # convert /tmp/tester.png -set colorspace gray - | convert -white-threshold 60% - /tmp/tester1.png
 # Capture2Text_CLI -i test.png --scale-factor=5 --blacklist=~
 output, _ = subprocess.Popen(['Capture2Text_CLI'],
