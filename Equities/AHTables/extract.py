ca9bf9d19bff08ce908ff544e3bd67383579defe
GSam <1924133+GSam@users.noreply.github.com>
2018-05-02 21:35:10 +1200

    Update extract.py

--- Equities/AHTables/extract.py
+++ Equities/AHTables/extract.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import subprocess
 
 url = os.environ.get("AHTABLEURL")
 
@@ -11,4 +12,9 @@ if len(sys.argv) != 3:
     print("You must supply two arguments")
 
 url = url.format(sys.argv[1], sys.argv[2])
+
+# convert /tmp/tester.png -set colorspace gray - | convert -white-threshold 60% - /tmp/tester1.png
+# Capture2Text_CLI -i test.png --scale-factor=5 --blacklist=~
+output, _ = subprocess.Popen(['Capture2Text_CLI'],
+                              stdout=subprocess.PIPE).communicate()
 print(url)
