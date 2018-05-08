6cca850cb86285b3b8b2ee9bb22e2ae9695893e1
Garming Sam <garming_sam@outlook.com>
2018-05-08 12:16:07 +1200

    AHTable: plan to use ghost-script

--- Equities/AHTables/ah_image.py
+++ Equities/AHTables/ah_image.py
@@ -1,1 +1,5 @@
-pass
+import subprocess
+
+def process_image(filename):
+    output, _ = subprocess.Popen(['Capture2Text_CLI'],
+                                  stdout=subprocess.PIPE).communicate()
--- Equities/AHTables/extract.py
+++ Equities/AHTables/extract.py
@@ -8,7 +8,7 @@ url = os.environ.get("AHTABLEURL")
 if url is None:
     print("Please set the AHTABLEURL environment variable")
     sys.exit(0)
-    
+
 if len(sys.argv) != 3:
     print("You must supply two arguments")
 
@@ -17,8 +17,8 @@ url = url.format(sys.argv[1], sys.argv[2])
 # extract using convert ... pdf image.png -crop 600x400+0x600
 # convert /tmp/tester.png -set colorspace gray - | convert -white-threshold 60% - /tmp/tester1.png
 # Capture2Text_CLI -i test.png --scale-factor=5 --blacklist=~
-output, _ = subprocess.Popen(['Capture2Text_CLI'],
-                              stdout=subprocess.PIPE).communicate()
+# gs -sDEVICE=pnggray -r1600  -o image.png AHTABLEURL
+# gs -sDEVICE=pnggray -r600 -g2900x3235 -o image.png -c -c "<</Install {-220 -170 translate}>> setpagedevice" -f  AHTABLEURL
 
 filename = ''
 ah_image.process_image(filename)
