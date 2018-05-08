bff1895b25dee85dcafd246c7166c8e767763af8
GSam <1924133+GSam@users.noreply.github.com>
2018-05-08 20:11:36 +1200

    Remove comments

--- Equities/AHTables/extract.py
+++ Equities/AHTables/extract.py
@@ -14,11 +14,6 @@ if len(sys.argv) != 3:
 
 url = url.format(sys.argv[1], sys.argv[2])
 
-# extract using convert ... pdf image.png -crop 600x400+0x600
-# convert /tmp/tester.png -set colorspace gray - | convert -white-threshold 60% - /tmp/tester1.png
-# Capture2Text_CLI -i test.png --scale-factor=5 --blacklist=~
-# gs -sDEVICE=pnggray -r1600  -o image.png AHTABLEURL
-
 output, _ = subprocess.Popen(['gs', '-s', 'DEVICE=pnggray', '-r', '600', '-g',
                               '2900x3235', '-o', output_gs, '-c',
                               '<</Install {-220 -170 translate}>> setpagedevice',
