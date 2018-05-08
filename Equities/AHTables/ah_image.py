7f0f4a647537f7fd549be4684abcc1b8c1bbcd5a
GSam <1924133+GSam@users.noreply.github.com>
2018-05-08 20:11:04 +1200

    Update ah_image.py

--- Equities/AHTables/ah_image.py
+++ Equities/AHTables/ah_image.py
@@ -1,5 +1,8 @@
 import subprocess
 
 def process_image(filename):
-    output, _ = subprocess.Popen(['Capture2Text_CLI'],
+    output, _ = subprocess.Popen(['Capture2Text_CLI', '-i', filename,
+                                  '--blacklist', '~', '--scale-factor', '1'],
                                   stdout=subprocess.PIPE).communicate()
+    # interpret output here
+    return ''
