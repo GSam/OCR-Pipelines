0c203a5ae112c1092dfb8271ffc7485e335c22a1
Garming Sam <garming_sam@outlook.com>
2018-05-11 07:48:11 +0000

    Disable GUI components when running on server

--- Equities/AHTables/ah_image.py
+++ Equities/AHTables/ah_image.py
@@ -1,7 +1,8 @@
 import subprocess
 
 def process_image(filename):
-    output, _ = subprocess.Popen(['./Capture2Text_CLI', '-i', filename,
+    output, _ = subprocess.Popen(['./Capture2Text_CLI', '-platform',
+                                  'offscreen', '-i', filename,
                                   '--blacklist', '~|\\V', '--scale-factor', '1'],
                                   stdout=subprocess.PIPE).communicate()
     # interpret output here
