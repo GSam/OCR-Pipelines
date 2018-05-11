d231b7525c09d9ef30d2c96d984812a45952c6f5
Garming Sam <garming_sam@outlook.com>
2018-05-11 08:01:52 +0000

    Trying to deal with |\/| again...

--- Equities/AHTables/ah_image.py
+++ Equities/AHTables/ah_image.py
@@ -1,9 +1,9 @@
 import subprocess
 
-def process_image(filename):
+def process_image(filename, scale=1.0):
     output, _ = subprocess.Popen(['./Capture2Text_CLI', '-platform',
                                   'offscreen', '-i', filename,
-                                  '--blacklist', '~|\\V', '--scale-factor', '1'],
+                                  '--blacklist', '~|\\V', '--scale-factor', str(scale)],
                                   stdout=subprocess.PIPE).communicate()
     # interpret output here
     print output
