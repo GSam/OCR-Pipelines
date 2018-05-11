318027466c669668553e9aad4009a53cca09d119
Garming Sam <garming_sam@outlook.com>
2018-05-11 07:56:16 +0000

    Add tessdata to setup

--- build-capture2text.sh
+++ build-capture2text.sh
@@ -1,2 +1,5 @@
 QT_SELECT=5 qmake Capture2Text/Capture2Text.pro -d
 make
+wget "https://github.com/GSam/Capture2Text/releases/download/Prototype/English.zip"
+unzip -o "English.zip" -d "tessdata"
+rm "English.zip"
