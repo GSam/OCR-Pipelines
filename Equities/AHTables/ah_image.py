d592352377de00f64d08017c40a966b633492bb5
Garming Sam <garming_sam@outlook.com>
2018-05-11 20:06:24 +1200

    Turn |\/| into M

--- Equities/AHTables/ah_image.py
+++ Equities/AHTables/ah_image.py
@@ -2,7 +2,7 @@ import subprocess
 
 def process_image(filename):
     output, _ = subprocess.Popen(['./Capture2Text_CLI', '-i', filename,
-                                  '--blacklist', '~', '--scale-factor', '1'],
+                                  '--blacklist', '~|\\V', '--scale-factor', '1'],
                                   stdout=subprocess.PIPE).communicate()
     # interpret output here
     print output
